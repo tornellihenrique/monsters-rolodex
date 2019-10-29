710253bdb7876a85ba7334b45bed6dc0115ba2e2
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2019-10-29 13:31:59 -0300

    Updates

---
+++ precache-manifest.de100979977a08a75ad9e876c5205e73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd67b2c21d8e32e8b2799e9b91cf1963",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2af1012cdddf8a653ab2",
+    "url": "/monsters-rolodex/static/css/main.8c45dc17.chunk.css"
+  },
+  {
+    "revision": "c4de08c8e1140d25c0ae",
+    "url": "/monsters-rolodex/static/js/2.5905bb17.chunk.js"
+  },
+  {
+    "revision": "2af1012cdddf8a653ab2",
+    "url": "/monsters-rolodex/static/js/main.fb7a1cce.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
